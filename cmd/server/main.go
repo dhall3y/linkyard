1e6f4375db0d1ffdf0205d29bc3345dfd666a533
dhall3y <dhalley@disroot.org>
2026-02-15 22:34:57 +0900

    add db connection with pgx

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -14,10 +16,28 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Unable to connect to db: %v", err)
+	}
+	defer conn.Close(context.Background())
 
-	secret := os.Getenv("POSTGRES_PASSWORD")
+	var name string
+	var url string
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(secret)
+		ctx := r.Context()
+		row := conn.QueryRow(ctx, "SELECT name, url FROM links")
+
+		err := row.Scan(&name, &url)
+		if err != nil {
+			if err == pgx.ErrNoRows {
+				fmt.Println("no rows in table links")
+			}
+			return
+		}
+		fmt.Println(name)
+		fmt.Println(url)
 	})
 	http.ListenAndServe(":8000", nil)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,24 @@ module linkyard
 
 go 1.25.5
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+)
+
+require (
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/jackc/pgx/v5 v5.8.0
+	github.com/lib/pq v1.11.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+	golang.org/x/crypto v0.48.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
+)
