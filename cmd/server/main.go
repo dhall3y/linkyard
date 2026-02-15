9f555d4c7765034f06e07a66b7c9b6133980e0c9
dhall3y <dhalley@disroot.org>
2026-02-15 19:20:51 +0900

    testing godotenv

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	secret := os.Getenv("POSTGRES_PASSWORD")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("init")
+		fmt.Println(secret)
 	})
 	http.ListenAndServe(":8000", nil)
 }
